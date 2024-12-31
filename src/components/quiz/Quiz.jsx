5b20b895841349e801f88865a55957b5cca19622
Ahmad Maimun <ahmadmaimun.wdd@gmail.com>
2024-12-31 11:32:20 +0600

    Updated

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -4,8 +4,8 @@ import "./Quiz.css";
 
 const Quiz = () => {
   const [index, setIndex] = useState(0);
-  const [questions, setQuestions] = useState(data[index]);
   const [lock, setLock] = useState(false);
+  const [score, setScore] = useState(0);
 
   const option1 = useRef(null);
   const option2 = useRef(null);
@@ -15,15 +15,28 @@ const Quiz = () => {
   const optionArr = [option1, option2, option3, option4];
 
   const checkAns = (e, ans) => {
-    if (lock === false) {
-      if (questions.ans === ans) {
+    if (!lock) {
+      if (data[index].ans === ans) {
         e.target.classList.add("correct");
-        setLock(true);
+        setScore((prev) => prev + 1);
       } else {
         e.target.classList.add("wrong");
-        setLock(true);
-        optionArr[questions.ans - 1].current.classList.add("correct");
+        optionArr[data[index].ans - 1].current.classList.add("correct");
       }
+      setLock(true);
+    }
+  };
+
+  const next = () => {
+    if (lock) {
+      setIndex((prevIndex) => prevIndex + 1);
+      setLock(false);
+
+      // Reset classes for all options
+      optionArr.forEach((option) => {
+        option.current.classList.remove("wrong");
+        option.current.classList.remove("correct");
+      });
     }
   };
 
@@ -32,7 +45,7 @@ const Quiz = () => {
       <h1 className="text-3xl font-bold">Quiz App</h1>
       <hr className="h-[2px] border-none bg-[#707070]" />
       <h2 className="text-[22px] font-medium">
-        {index + 1}. {questions.question}
+        {index + 1}. {data[index].question}
       </h2>
       <ul>
         <li
@@ -40,34 +53,39 @@ const Quiz = () => {
           onClick={(e) => checkAns(e, 1)}
           className="flex items-center h-[70px] pl-[15px] border border-[#686868] rounded-[8px] mb-[20px] text-[20px] cursor-pointer"
         >
-          {questions.option1}
+          {data[index].option1}
         </li>
         <li
           ref={option2}
           onClick={(e) => checkAns(e, 2)}
           className="flex items-center h-[70px] pl-[15px] border border-[#686868] rounded-[8px] mb-[20px] text-[20px] cursor-pointer"
         >
-          {questions.option2}
+          {data[index].option2}
         </li>
         <li
           ref={option3}
           onClick={(e) => checkAns(e, 3)}
           className="flex items-center h-[70px] pl-[15px] border border-[#686868] rounded-[8px] mb-[20px] text-[20px] cursor-pointer"
         >
-          {questions.option3}
+          {data[index].option3}
         </li>
         <li
           ref={option4}
           onClick={(e) => checkAns(e, 4)}
           className="flex items-center h-[70px] pl-[15px] border border-[#686868] rounded-[8px] mb-[20px] text-[20px] cursor-pointer"
         >
-          {questions.option4}
+          {data[index].option4}
         </li>
       </ul>
-      <button className="m-auto w-[250px] h-[65px] bg-[#553f9a] text-white text-[25px] font-medium rounded-[8px]">
+      <button
+        onClick={next}
+        className="m-auto w-[250px] h-[65px] bg-[#553f9a] text-white text-[25px] font-medium rounded-[8px]"
+      >
         Next
       </button>
-      <div className="m-auto text-[18px]">1 of 5 questions</div>
+      <div className="m-auto text-[18px]">
+        {index + 1} of {data.length} questions
+      </div>
     </div>
   );
 };
